b1555b5faa7d46c2d2b0c0e782c44d13e4a5fc36
Arezim Legion <arezimt@gmail.com>
2025-12-04 13:54:49 +0100

    minor

--- contracts/ConsentManager.sol
+++ contracts/ConsentManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.28;
 
 /// @notice Minimal interface to talk to RewardToken
 interface IRewardToken {
--- contracts/IdentityRegistry.sol
+++ contracts/IdentityRegistry.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.28;
 
 //Registers users and their roles and stores hashed attributes.
 contract IdentityRegistry {
